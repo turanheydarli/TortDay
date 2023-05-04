62b029fe524ab2f381a78e5aa39bf8c5cf724ab5
Turan Heydarli <turan.heydarli@code.edu.az>
2023-05-04 19:43:39 +0400

    First initialize

---
+++ 3DPractice/Assets/Sctipts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float smoothTurnTime;
+
+    private Vector3 _direction;
+    private Rigidbody _rigidbody;
+    private float _currentTurnAngle;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        _direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (_direction.magnitude > 0.01f)
+        {
+            float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentTurnAngle,
+                smoothTurnTime);
+
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            _rigidbody.MovePosition(transform.position + (_direction.normalized * (speed * Time.deltaTime)));
+        }
+    }
+}
